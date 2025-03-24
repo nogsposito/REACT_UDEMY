7da32e5df9cff5a7441fcca8f093e7f60d8f1554
nogsposito <viniciussposito3103@gmail.com>
2025-03-24 14:57:49 -0300

    primeiro

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+// components
+import FirstComponnent from './components/FirstComponnent';
+// styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>fundamentos react</h1>
+      <FirstComponnent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponnent.js
@@ -0,0 +1,9 @@
+const FirstComponnent = () => {
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponnent;
